15a50ab3d19549b31e02f90d5adf73c865c871b3
Dec Burns <dec@decs-mbp.home>
2024-04-14 10:42:19 +0100

    bulma

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -6,9 +6,16 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [NgIf],
   template: `
-   <button>{{ value }}</button>
+   <button class="button is-dark is-fullwidth" *ngIf="!value">{{ value }}</button>
+   <button class="button is-success is-fullwidth" status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button class="button is-info is-fullwidth" status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: `
+  button {
+    width: 100%;
+    height: 100%;
+    font-size: 40px;
+  }
 `
 })
 export class SquareComponent {
